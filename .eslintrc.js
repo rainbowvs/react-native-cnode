a84c1ed1dedc8cd16de6d2474cf664bd414a844f
rainbowvs <527532711@qq.com>
2018-09-19 00:27:30 +0800

    新增部分规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,11 @@ module.exports = {
     "linebreak-style": ["off", "windows"], // 换行
     "react/jsx-filename-extension": [0], // 文件扩展名
     "comma-dangle": ["error", "never"], // 句末逗号
-    'no-console': 'off' // console
+    'no-console': 'off', // console
+    "react/forbid-prop-types": [0], // 类型验证
+    "import/no-extraneous-dependencies": [0], // 依赖
+    "arrow-body-style": [0], // 箭头函数括号
+    "import/prefer-default-export": [0] // 默认导出
   },
   "globals": {
     "__DEV__": true,
