cf0379ff1ef5945e3a9a59765e56f20625d5d744
rainbowvs <527532711@qq.com>
2018-09-19 00:49:16 +0800

    封装 列表公共View组件

---
+++ src/coms/Tab.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class Tab extends React.Component {
+  componentDidMount() {
+    //
+  }
+
+  render() {
+    return (
+      <Text>1</Text>
+    );
+  }
+}
