d80b29ec35f39742e66a9187e24672dcc60af03b
rainbowvs <527532711@qq.com>
2018-09-19 00:41:37 +0800

    新增 主页基本布局

--- App.js
+++ App.js
@@ -1,58 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
+import PropTypes from 'prop-types';
+import Header from './src/coms/Header';
+import ViewUtils from './src/coms/ViewUtils';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const theme = {
+  mainColor: '#80bd01',
+  secondaryColor: 'mintcream'
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Header
+          title="Cnode"
+          statusBar={{ backgroundColor: theme.mainColor }}
+          leftButton={ViewUtils.getIconButton('menu', { marginLeft: 10 }, () => {
+            navigation.openDrawer();
+          })}
+          rightButton={ViewUtils.getIconButton('edit-square', { marginRight: 10 }, () => {
+            //
+          })}
+        />
+        <ScrollableTabView
+          tabBarBackgroundColor={theme.mainColor}
+          tabBarActiveTextColor="white"
+          tabBarInactiveTextColor={theme.secondaryColor}
+          tabBarUnderlineStyle={{
+            backgroundColor: 'white',
+            height: 3
+          }}
+          renderTabBar={() => (
+            <ScrollableTabBar
+              style={{ height: 44 }}
+              tabStyle={{ height: 44 }}
+            />)
+          }
+        >
+          <Text tabLabel="Java">Java1</Text>
+          <Text tabLabel="C">C</Text>
+          <Text tabLabel="C++">C++</Text>
+          <Text tabLabel="Ruby">Ruby</Text>
+          <Text tabLabel="Ruby1">Ruby1</Text>
+          <Text tabLabel="Ruby2">Ruby2</Text>
+          <Text tabLabel="Ruby3">Ruby3</Text>
+          <Text tabLabel="Ruby4">Ruby4</Text>
+          <Text tabLabel="Ruby5">Ruby5</Text>
+          <Text tabLabel="Ruby6">Ruby6</Text>
+        </ScrollableTabView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+App.propTypes = {
+  navigation: PropTypes.object.isRequired
+};
