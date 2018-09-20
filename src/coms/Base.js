eca0b39291864a118adbb55da599a42c28a9ba55
rainbowvs <527532711@qq.com>
2018-09-21 01:43:25 +0800

    封装 DeviceEventEmitter监听基类, 配合动态改变主题

---
+++ src/coms/Base.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DeviceEventEmitter } from 'react-native';
+
+export default class Base extends React.Component {
+  componentDidMount() {
+    this.listener = DeviceEventEmitter.addListener('CHANGE_THEME', (params) => {
+      this.setState(() => ({
+        themeColor: params
+      }));
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.listener) {
+      this.listener.remove();
+    }
+  }
+}
