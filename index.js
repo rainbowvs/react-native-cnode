a100294b47a7955cb0f80e81f7d85888bf639a05
rainbowvs <527532711@qq.com>
2018-09-21 01:35:20 +0800

    入口改为StackNav导航组件

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import { StackNav } from './src/coms/Navigation';
-// import App from './App';
 
 AppRegistry.registerComponent('rnCnode', () => StackNav);
