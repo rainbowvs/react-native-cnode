d7cb4dfa114d7063808ab22d5550fb75a6e23d11
rainbowvs <527532711@qq.com>
2018-09-21 01:34:17 +0800

    将App组件拆分出Home首页, App组件暂定为启动入口

--- App.js
+++ App.js
@@ -1,73 +1,31 @@
 import React from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
 import PropTypes from 'prop-types';
-import Header from './src/coms/Header';
-import ViewUtils from './src/coms/ViewUtils';
-
-const theme = {
-  mainColor: '#80bd01',
-  secondaryColor: 'mintcream'
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
+import ThemeDao from './expand/dao/ThemeDao';
 
 export default class App extends React.Component {
   componentDidMount() {
-    SplashScreen.hide();
+    const { navigation } = this.props;
+    new ThemeDao().getTheme().then((res) => {
+      const resetAction = StackActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({
+            routeName: 'Home',
+            params: {
+              themeColor: res
+            }
+          })
+        ]
+      });
+      navigation.dispatch(resetAction);
+      SplashScreen.hide();
+    });
   }
 
   render() {
-    const { navigation } = this.props;
-    return (
-      <View style={styles.container}>
-        <Header
-          title="Cnode"
-          statusBar={{ backgroundColor: theme.mainColor }}
-          leftButton={ViewUtils.getIconButton('menu', { marginLeft: 10 }, () => {
-            navigation.openDrawer();
-          })}
-          rightButton={ViewUtils.getIconButton('edit-square', { marginRight: 10 }, () => {
-            //
-          })}
-        />
-        <ScrollableTabView
-          tabBarBackgroundColor={theme.mainColor}
-          tabBarActiveTextColor="white"
-          tabBarInactiveTextColor={theme.secondaryColor}
-          tabBarUnderlineStyle={{
-            backgroundColor: 'white',
-            height: 3
-          }}
-          renderTabBar={() => (
-            <ScrollableTabBar
-              style={{ height: 44 }}
-              tabStyle={{ height: 44 }}
-            />)
-          }
-        >
-          <Text tabLabel="Java">Java1</Text>
-          <Text tabLabel="C">C</Text>
-          <Text tabLabel="C++">C++</Text>
-          <Text tabLabel="Ruby">Ruby</Text>
-          <Text tabLabel="Ruby1">Ruby1</Text>
-          <Text tabLabel="Ruby2">Ruby2</Text>
-          <Text tabLabel="Ruby3">Ruby3</Text>
-          <Text tabLabel="Ruby4">Ruby4</Text>
-          <Text tabLabel="Ruby5">Ruby5</Text>
-          <Text tabLabel="Ruby6">Ruby6</Text>
-        </ScrollableTabView>
-      </View>
-    );
+    return null;
   }
 }
 
