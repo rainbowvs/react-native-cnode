47063cfd57c41640fd085822e232154c8772623a
rainbowvs <527532711@qq.com>
2018-09-22 00:29:34 +0800

    新增 部分规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,16 @@ module.exports = {
   },
   "rules": {
     "linebreak-style": ["off", "windows"], // 换行
-    "react/jsx-filename-extension": [0], // 文件扩展名
+    "react/jsx-filename-extension": 0, // 文件扩展名
     "comma-dangle": ["error", "never"], // 句末逗号
-    'no-console': 'off', // console
-    "react/forbid-prop-types": [0], // 类型验证
-    "import/no-extraneous-dependencies": [0], // 依赖
-    "arrow-body-style": [0], // 箭头函数括号
-    "import/prefer-default-export": [0] // 默认导出
+    'no-console': 0, // console
+    "react/forbid-prop-types": 0, // 类型验证
+    "import/no-extraneous-dependencies": 0, // 依赖
+    "arrow-body-style": 0, // 箭头函数返回值格式
+    "arrow-parens": ["error", "as-needed"], // 箭头函数参数格式
+    "import/prefer-default-export": 0, // 默认导出
+    "no-restricted-syntax": 0, //不允许指定（即用户定义）语法。
+    "guard-for-in": 0 // for in 遍历
   },
   "globals": {
     "__DEV__": true,
