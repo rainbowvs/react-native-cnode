453124ff81800c5cdf294ed4bc613a595c885989
rainbowvs <527532711@qq.com>
2018-09-22 00:34:58 +0800

    优化 代码规范

--- src/coms/Base.js
+++ src/coms/Base.js
@@ -3,7 +3,7 @@ import { DeviceEventEmitter } from 'react-native';
 
 export default class Base extends React.Component {
   componentDidMount() {
-    this.listener = DeviceEventEmitter.addListener('CHANGE_THEME', (params) => {
+    this.listener = DeviceEventEmitter.addListener('CHANGE_THEME', params => {
       this.setState(() => ({
         themeColor: params
       }));
