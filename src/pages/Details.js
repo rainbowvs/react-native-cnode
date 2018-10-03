082c973e35ada4f6b220d85cc5f4e21e7ff5e68c
rainbowvs <527532711@qq.com>
2018-10-04 00:12:47 +0800

    添加 详情页头部

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import {
-  Text,
   View,
-  Button
+  ScrollView
 } from 'react-native';
 import PropTypes from 'prop-types';
+import Header from '../coms/Header';
+import Base from '../coms/Base';
+import ViewUtils from '../coms/ViewUtils';
 
-export default class Details extends React.Component {
+export default class Details extends Base {
+  constructor(props) {
+    super(props);
+    const { navigation } = this.props;
+    const themeColor = navigation.getParam('themeColor');
+    console.log(navigation);
+    this.state = {
+      themeColor,
+      list: [],
+      isLoading: false,
+      topicMd: navigation.getParam('item').content
+    };
+  }
+
   componentDidMount() {
+    super.componentDidMount();
     return 1;
   }
 
   render() {
     const { navigation } = this.props;
+    const { themeColor, topicMd } = this.state;
     return (
       <View>
-        <Text>Details</Text>
-        <Button
-          title="Home"
-          onPress={() => {
+        <Header
+          title="详情"
+          themeColor={themeColor}
+          leftButton={ViewUtils.getIconButton('arrowleft', { marginLeft: 10 }, () => {
             navigation.goBack();
-          }}
+          })}
         />
+        <ScrollView>
+          <View></View>
+        </ScrollView>
       </View>
     );
   }
