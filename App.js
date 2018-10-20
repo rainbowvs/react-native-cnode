dfde14dc28849d9a9d0cc6195e4355010dbf1416
rainbowvs <527532711@qq.com>
2018-10-20 23:34:15 +0800

    新增 初始化时读取用户数据

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import SplashScreen from 'react-native-splash-screen';
 import { StackActions, NavigationActions } from 'react-navigation';
 import PropTypes from 'prop-types';
 import ThemeDao from './expand/dao/ThemeDao';
+import UserDao from './expand/dao/UserDao';
 
 export default class App extends React.Component {
   componentDidMount() {
     const { navigation } = this.props;
-    new ThemeDao().getTheme().then(res => {
+    Promise.all([new ThemeDao().getTheme(), new UserDao().getUser()]).then(res => {
+      const [themeColor, userInfo] = res;
       const resetAction = StackActions.reset({
         index: 0,
         actions: [
           NavigationActions.navigate({
             routeName: 'Home',
             params: {
-              themeColor: res
+              themeColor,
+              userInfo
             }
           })
         ]
