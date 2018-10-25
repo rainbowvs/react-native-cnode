687d56d254829344964df0f724bc64b963215bc1
rainbowvs <527532711@qq.com>
2018-10-26 00:00:02 +0800

    优化 头部样式, 修改 完善功能.

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,6 +4,7 @@ import {
   Text,
   Image,
   TouchableOpacity,
+  StatusBar,
   StyleSheet,
   Dimensions,
   Platform
@@ -18,15 +19,20 @@ import { getTimeInterval } from '../utils/dateUtils';
 
 const window = Dimensions.get('window');
 
+const IOS = Platform.OS === 'ios';
+const STATUSBAR_HEIGHT = IOS ? 20 : StatusBar.currentHeight;
+
 const AVATAR_SIZE = 120;
 const PARALLAX_HEADER_HEIGHT = 350;
-const STICKY_HEADER_HEIGHT = 70;
+const STICKY_HEADER_HEIGHT = 50 + STATUSBAR_HEIGHT;
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
   stickySection: {
+    paddingTop: STATUSBAR_HEIGHT,
     height: STICKY_HEADER_HEIGHT,
     width: window.width,
     justifyContent: 'center'
@@ -38,10 +44,10 @@ const styles = StyleSheet.create({
   },
   fixedSection: {
     position: 'absolute',
-    top: 0,
+    top: STATUSBAR_HEIGHT,
     left: 0,
     paddingLeft: 10,
-    height: STICKY_HEADER_HEIGHT,
+    height: STICKY_HEADER_HEIGHT - STATUSBAR_HEIGHT,
     justifyContent: 'center'
   },
   backgroundMask: {
@@ -65,12 +71,23 @@ const styles = StyleSheet.create({
     height: AVATAR_SIZE,
     borderRadius: AVATAR_SIZE / 2
   },
-  sectionSpeakerText: {
+  userName: {
     color: '#fff',
     fontSize: 24,
     paddingVertical: 5
   },
+  githubAndCredits: {
+    width: window.width,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  githubAndCreditsTouchAble: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   credits: {
+    marginRight: 20,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
@@ -84,6 +101,96 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     color: '#fff',
     fontSize: 20
+  },
+  githubIcon: {
+    marginRight: 10,
+    fontSize: 26,
+    color: '#fff'
+  },
+  githubName: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  registerDate: {
+    fontSize: 18,
+    color: '#fff'
+  },
+  tab: {
+    flex: 1,
+    width: window.width
+  },
+  tabTouchable: {
+    flex: 1,
+    height: 60,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0'
+  },
+  tabLabel: {
+    fontSize: 20,
+    textAlignVertical: 'center'
+  },
+  tabIcon: {
+    fontSize: 16,
+    color: '#aaa'
+  },
+  itemTouchable: {
+    flex: 1,
+    height: 80,
+    justifyContent: 'center',
+    backgroundColor: '#fafafa',
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff'
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10
+  },
+  itemAvatar: {
+    borderRadius: 20,
+    width: 40,
+    height: 40
+  },
+  itemTopic: {
+    marginLeft: 10,
+    flex: 1,
+    width: window.width
+  },
+  itemTopicTitle: {
+    fontSize: 18,
+    color: '#333'
+  },
+  itemTopicInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  itemAuthorName: {
+    fontSize: 16,
+    color: '#666',
+    textAlignVertical: 'center'
+  },
+  itemTopicReplyDate: {
+    fontSize: 16,
+    color: '#777',
+    textAlignVertical: 'center'
+  },
+  emptyItem: {
+    flex: 1,
+    paddingHorizontal: 10,
+    height: 80,
+    justifyContent: 'center',
+    backgroundColor: '#fafafa',
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff'
+  },
+  emptyItemText: {
+    fontSize: 16,
+    textAlign: 'center',
+    textAlignVertical: 'center'
   }
 });
 
@@ -169,14 +276,14 @@ export default class User extends React.Component {
   renderTab(userInfo, type, label) {
     const typeName = type === 0 ? 'showRecentReplies' : 'showrecentTopics';
     return (
-      <View key={type} style={{ flex: 1, alignSelf: 'stretch' }}>
+      <View key={type} style={styles.tab}>
         <TouchableOpacity
           activeOpacity={0.2}
           onPress={() => this.tabClick(type, typeName)}
-          style={{ flex: 1, height: 60, paddingHorizontal: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#f0f0f0' }}
+          style={styles.tabTouchable}
         >
-          <Text style={{ fontSize: 20, textAlignVertical: 'center' }}>{label}</Text>
-          <IconFont style={{ color: '#aaa' }} name={`${this.state[typeName] ? 'up' : 'down'}`} />
+          <Text style={styles.tabLabel}>{label}</Text>
+          <IconFont style={styles.tabIcon} name={`${this.state[typeName] ? 'up' : 'down'}`} />
         </TouchableOpacity>
         {this.state[typeName] && this.renderList(userInfo, type)}
       </View>
@@ -187,13 +294,9 @@ export default class User extends React.Component {
     let list = null;
     if (userInfo) {
       if (type === 0) {
-        list = userInfo.recent_replies.map(v => {
-          return this.renderItem(v);
-        });
+        list = userInfo.recent_replies.map(v => this.renderItem(v));
       } else {
-        list = userInfo.recent_topics.map(v => {
-          return this.renderItem(v);
-        });
+        list = userInfo.recent_topics.map(v => this.renderItem(v));
       }
       list = list.length === 0 ? this.renderEmptyItem() : list;
     } else {
@@ -204,8 +307,8 @@ export default class User extends React.Component {
 
   renderEmptyItem() {
     return (
-      <View style={{ paddingHorizontal: 10, flex: 1, height: 80, justifyContent: 'center', backgroundColor: '#fafafa', borderBottomWidth: 1, borderBottomColor: '#fff' }}>
-        <Text style={{ fontSize: 16, textAlign: 'center', textAlignVertical: 'center' }}>没有更多数据</Text>
+      <View style={styles.emptyItem}>
+        <Text style={styles.emptyItemText}>没有更多数据</Text>
       </View>
     );
   }
@@ -217,18 +320,18 @@ export default class User extends React.Component {
       <TouchableOpacity
         key={item.id}
         activeOpacity={0.2}
-        style={{ flex: 1, height: 80, justifyContent: 'center', backgroundColor: '#fafafa', borderBottomWidth: 1, borderBottomColor: '#fff' }}
+        style={styles.itemTouchable}
         onPress={() => {
           navigation.push('Details', { themeColor, topicId: item.id });
         }}
       >
-        <View style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10 }}>
-          <Image style={{ borderRadius: 20, width: 40, height: 40 }} source={{ uri: item.author.avatar_url }} />
-          <View style={{ marginLeft: 10, flex: 1, alignSelf: 'stretch' }}>
-            <Text style={{ fontSize: 18, color: '#333' }} ellipsizeMode="tail" numberOfLines={1}>{item.title}</Text>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text style={{ fontSize: 16, color: '#666', textAlignVertical: 'center' }}>{item.author.loginname}</Text>
-              <Text style={{ fontSize: 16, color: '#777', textAlignVertical: 'center' }}>{getTimeInterval(item.last_reply_at)}</Text>
+        <View style={styles.itemContainer}>
+          <Image style={styles.itemAvatar} source={{ uri: item.author.avatar_url }} />
+          <View style={styles.itemTopic}>
+            <Text style={styles.itemTopicTitle} ellipsizeMode="tail" numberOfLines={1}>{item.title}</Text>
+            <View style={styles.itemTopicInfo}>
+              <Text style={styles.itemAuthorName}>{item.author.loginname}</Text>
+              <Text style={styles.itemTopicReplyDate}>{getTimeInterval(item.last_reply_at)}</Text>
             </View>
           </View>
         </View>
@@ -256,55 +359,39 @@ export default class User extends React.Component {
     return (
       <View style={styles.container}>
         <ParallaxScrollView
-
           backgroundColor={themeColor}
           contentBackgroundColor="#fff"
           contentContainerStyle={{ flex: 1 }}
           stickyHeaderHeight={STICKY_HEADER_HEIGHT}
           parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
           backgroundSpeed={10}
-
-          renderBackground={() => (
-            <View key="background">
-              {/* 背景 */}
-              <Image
-                style={styles.backgroundImg}
-                source={require('../../statics/imgs/bg_user.jpg')}
-              />
-              <View style={styles.backgroundMask} />
-            </View>
-          )}
-
+          renderBackground={() => {
+            /* 背景 */
+            return (
+              <View key="background">
+                <Image
+                  style={styles.backgroundImg}
+                  source={require('../../statics/imgs/bg_user.jpg')}
+                />
+                <View style={styles.backgroundMask} />
+              </View>
+            );
+          }}
           renderForeground={() => {
-            const avatarSource = userInfo === null ? require('../../statics/imgs/bg_avatar_default.jpg') : { uri: userInfo.avatar_url };
+            /* 头像 用户名 */
+            const avatarSource = userInfo === null
+              ? require('../../statics/imgs/bg_avatar_default.jpg')
+              : { uri: userInfo.avatar_url };
             return (
               <View key="parallax-header" style={styles.parallaxHeader}>
-                {/* 头像, 用户名 */}
                 <Image
                   style={styles.avatar}
                   source={avatarSource}
                 />
-                <Text style={styles.sectionSpeakerText}>
+                <Text style={styles.userName}>
                   {userName}
                 </Text>
-                <Text style={{ fontSize: 18, color: '#fff' }}>
-                  注册时间：
-                  {userInfo === null ? 'loading' : userInfo.create_at.substring(0, 10)}
-                </Text>
-                <View style={{ alignSelf: 'stretch', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
-                  <TouchableOpacity
-                    activeOpacity={0.2}
-                    onPress={() => {}}
-                    style={{ flexDirection: 'row', alignItems: 'center' }}
-                  >
-                    <IconFont
-                      name="github-fill"
-                      style={{ marginRight: 10, fontSize: 26, color: '#fff' }}
-                    />
-                    <Text style={{ fontSize: 20, color: '#fff' }}>
-                      {userInfo === null ? 'loading' : userInfo.githubUsername}
-                    </Text>
-                  </TouchableOpacity>
+                <View style={styles.githubAndCredits}>
                   <View style={styles.credits}>
                     <IconFont
                       name="trophy"
@@ -314,30 +401,53 @@ export default class User extends React.Component {
                       {userInfo === null ? 0 : userInfo.score}
                     </Text>
                   </View>
+                  <TouchableOpacity
+                    activeOpacity={0.2}
+                    onPress={() => {
+                      if (userInfo) {
+                        navigation.navigate('XWebview', { themeColor, uri: `https://github.com/${userInfo.githubUsername}`, title: `Github / ${userInfo.githubUsername}` });
+                      } else {
+                        Toast('正在获取数据...');
+                      }
+                    }}
+                    style={styles.githubAndCreditsTouchAble}
+                  >
+                    <IconFont
+                      name="github-fill"
+                      style={styles.githubIcon}
+                    />
+                    <Text style={styles.githubName}>
+                      {userInfo === null ? 'loading' : userInfo.githubUsername}
+                    </Text>
+                  </TouchableOpacity>
                 </View>
+                <Text style={styles.registerDate}>
+                  注册时间：
+                  {userInfo === null ? 'loading' : userInfo.create_at.substring(0, 10)}
+                </Text>
               </View>
             );
           }}
-
-          renderStickyHeader={() => (
-            <View key="sticky-header" style={styles.stickySection}>
-              {/* 标题 */}
-              <Text style={styles.stickySectionText}>{userName}</Text>
-            </View>
-          )}
-
-          renderFixedHeader={() => (
-            <View key="fixed-header" style={styles.fixedSection}>
-              {/* 返回键 */}
-              {ViewUtils.getIconButton('arrowleft', {}, () => {
-                navigation.goBack();
-              })}
-            </View>
-          )}
+          renderStickyHeader={() => {
+            /* 标题名 */
+            return (
+              <View key="sticky-header" style={styles.stickySection}>
+                <Text style={styles.stickySectionText}>{userName}</Text>
+              </View>
+            );
+          }}
+          renderFixedHeader={() => {
+            /* 返回键 */
+            return (
+              <View key="fixed-header" style={styles.fixedSection}>
+                {ViewUtils.getIconButton('arrowleft', {}, () => {
+                  navigation.goBack();
+                })}
+              </View>
+            );
+          }}
         >
-          {tabs.map(v => {
-            return this.renderTab(userInfo, v.type, v.label);
-          })}
+          {tabs.map(v => this.renderTab(userInfo, v.type, v.label))}
         </ParallaxScrollView>
       </View>
     );
