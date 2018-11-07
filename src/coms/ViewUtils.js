5679930216a5ab82f8a368788a3072d13f12a918
rainbowvs <527532711@qq.com>
2018-11-07 17:30:50 +0800

    新增 getModalLoading方法

--- src/coms/ViewUtils.js
+++ src/coms/ViewUtils.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   View,
+  Modal,
   TouchableOpacity,
   ActivityIndicator,
   StyleSheet
@@ -11,6 +12,20 @@ const styles = StyleSheet.create({
   icon: {
     color: '#fff',
     fontSize: 26
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  modalContent: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
@@ -35,6 +50,27 @@ export default class ViewUtils {
           <View style={style}>
             <ActivityIndicator color={themeColor} size="large" />
           </View>
+        )
+        : null
+    );
+  }
+
+  static getModalLoading(flag, style, themeColor) {
+    return (
+      flag
+        ? (
+          <Modal
+            visible
+            transparent
+            animationType="slide"
+            onRequestClose={() => {}}
+          >
+            <View style={styles.modal}>
+              <View style={styles.modalContent}>
+                <ActivityIndicator color={themeColor} size="large" />
+              </View>
+            </View>
+          </Modal>
         )
         : null
     );
